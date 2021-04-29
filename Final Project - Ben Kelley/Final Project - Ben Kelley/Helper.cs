ca49e1b836d25756815cda540f5bddb779b958b7
bkelley361 <69215873+bkelley361@users.noreply.github.com>
2021-04-29 00:19:57 -0400

    Add files via upload

---
+++ Final Project - Ben Kelley/Final Project - Ben Kelley/Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace Final_Project___Ben_Kelley
+{
+    public abstract class Helper
+    {
+        /// <summary>
+        /// Helper parent class that starts the connection string and HttpClient
+        /// </summary>
+        string cs { get; set; }
+        static readonly HttpClient client = new HttpClient();
+    }
+}
